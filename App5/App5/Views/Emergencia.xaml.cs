a54bed801ccf9f079fe60f676d727b8d7e94e344
Samantha <samanthajanneth5@gmail.com>
2020-11-10 00:49:18 -0600

    agregue botones de emergencia

--- App5/App5/Views/Emergencia.xaml.cs
+++ App5/App5/Views/Emergencia.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,19 @@ namespace PrivBus.Views
         public Emergencia()
         {
             InitializeComponent();
+
         }
+
+
+        private async void Emergencia1_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensaje","Emergencia!!","Cerrar");
+        }
+        private async void Cancela1_Clicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Mensaje", "Se canceló la solicitud de emergencia", "Cerrar");
+        }
+
+
     }
 }
