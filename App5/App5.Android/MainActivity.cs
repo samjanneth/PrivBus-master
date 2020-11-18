b88e5d4e2e7a834435931341cae7ef870cd93b53
Samantha <samanthajanneth5@gmail.com>
2020-11-17 18:46:44 -0600

    Agregue en la pantalla de codigo el QR, te pide permiso para la cámara y ya lee QR "texto"

--- App5/App5.Android/MainActivity.cs
+++ App5/App5.Android/MainActivity.cs
@@ -22,13 +22,17 @@ namespace App5.Droid
             Xamarin.FormsGoogleMaps.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            global::ZXing.Net.Mobile.Forms.Android.Platform.Init();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            global::ZXing.Net.Mobile.Android.PermissionsHandler.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+           
         }
     }
 }
--- App5/App5/Views/Codigo.xaml.cs
+++ App5/App5/Views/Codigo.xaml.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
 
 namespace PrivBus.Views
 {
@@ -15,6 +16,29 @@ namespace PrivBus.Views
         public Codigo()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        private async void Scanner()
+        {
+            var scannerPage = new ZXingScannerPage();
+
+            scannerPage.Title = "Lector de QR";
+            scannerPage.OnScanResult += (result) =>
+             {
+                 scannerPage.IsScanning = false;
+                 Device.BeginInvokeOnMainThread(() =>
+                 {
+                     Navigation.PopAsync();
+                     DisplayAlert("Valor obtenido", result.Text, "OK");
+                 });
+             };
+            await Navigation.PushAsync(scannerPage);
+
         }
     }
 }
